b41525bce32e42679eb5b9d69e371fbe3cf342d2
Kadi <kadikabdulov@gmail.com>
2024-02-25 18:49:54 +0300

    sl1

--- app/js/index.js
+++ app/js/index.js
@@ -15,7 +15,7 @@ const swiper = new Swiper('.swiper', {
     keyboard:true
   });
   
-let x = document.body.querySelector('.swiper');
+let swp = document.body.querySelector('.swiper');
 const bd = document.body;
 
 function next(event){
@@ -23,34 +23,34 @@ function next(event){
     document.body.querySelector('.swiper-button-next').style.cssText = `
       transition:all 0.5s ease-in-out 0s;
       opacity:1;
-    `
+    `;
     document.body.querySelector('.swiper-button-prev').style.cssText = `
       transition:all 0.5s ease-in-out 0s;
       opacity:1;
-    `
+    `;
   }else if(event.target.className == 'swiper-button-next'){
     document.body.querySelector('.swiper-button-next').style.cssText = `
       opacity:1;
-    `
+    `;
     document.body.querySelector('.swiper-button-prev').style.cssText = `
       opacity:1;
-    `
+    `;
   }else if(event.target.className == 'swiper-button-prev'){
     document.body.querySelector('.swiper-button-next').style.cssText = `
       opacity:1;
-    `
+    `;
     document.body.querySelector('.swiper-button-prev').style.cssText = `
       opacity:1;
-    `
+    `;
   }else{
     document.body.querySelector('.swiper-button-next').style.cssText = `
       opacity:0;
-    `
+    `;
     document.body.querySelector('.swiper-button-prev').style.cssText = `
       opacity:0;
-    `
-  }
+    `;
+  };
 
-}
-x.addEventListener('mouseover',next)
-bd.addEventListener('mouseover',next)
+};
+swp.addEventListener('mouseover',next);
+bd.addEventListener('mouseover',next);
